a0bf88a0168522cc37d99e5fa12572d42216712c
Eduardo José <eduardojsc18@outlook.com>
2025-02-12 22:03:10 -0300

    ajustes

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -2,10 +2,16 @@ export default defineNuxtRouteMiddleware((to) => {
 
     const session = useSession()
 
-    // if(session.user.value){
-    //     if(to.path.startsWith('/auth') && !to.path.includes('/auth/confirmation')) {
-    //         return navigateTo({path:'/admin/dashboard'})
-    //     }
-    // }
+    if (!session.isLogged.value) {
+        if (to.path === '/auth/login') {
+            return
+        }
+    }
+
+    if(session.user.value){
+        if(to.path.startsWith('/auth') && !to.path.includes('/auth/confirmation')) {
+            return navigateTo({path:'/admin/dashboard'})
+        }
+    }
 
 })
