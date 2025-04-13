5926cbe1480f2a0b222af847767191878bd8bc69
Eduardo José <eduardojsc18@outlook.com>
2025-04-13 18:55:06 -0300

    no message

--- supabase/migrations/20240303223419_create_function_add_user_to_profile.sql
+++ supabase/migrations/20240303223419_create_function_add_user_to_profile.sql
@@ -1,16 +1,58 @@
-
 create
-  or replace function add_new_user_to_profile_function()
-  returns trigger as $$
+or replace function add_new_user_to_profile_function()
+returns trigger as $$
+declare
+auth_provider text;
 begin
-  insert into public.profiles (id, email, username, name, avatar_url)
-  values (
-    new.id,
-    new.email,
-    new.raw_user_meta_data->>'user_name',
-    new.raw_user_meta_data->>'name',
-    new.raw_user_meta_data->>'avatar_url'
-  );
-  return new;
+  auth_provider := new.raw_user_meta_data->>'provider';
+
+  if auth_provider = 'apple' then
+    insert into public.profiles (id, email, username, name, avatar_url)
+    values (
+      new.id,
+      new.email,
+      new.raw_user_meta_data->>'email',
+      new.raw_user_meta_data->>'name',
+      new.raw_user_meta_data->>'avatar_url'
+    );
+  elsif auth_provider = 'google' then
+    insert into public.profiles (id, email, username, name, avatar_url)
+    values (
+      new.id,
+      new.email,
+      new.raw_user_meta_data->>'email',
+      new.raw_user_meta_data->>'name',
+      new.raw_user_meta_data->>'picture'
+    );
+  elsif auth_provider = 'facebook' then
+    insert into public.profiles (id, email, username, name, avatar_url)
+    values (
+      new.id,
+      new.email,
+      new.raw_user_meta_data->>'email',
+      new.raw_user_meta_data->>'name',
+      new.raw_user_meta_data->>'picture'
+    );
+  elsif auth_provider = 'email' then
+    insert into public.profiles (id, email, username, name, avatar_url)
+    values (
+      new.id,
+      new.email,
+      new.raw_user_meta_data->>'user_name',
+      new.raw_user_meta_data->>'name',
+      new.raw_user_meta_data->>'avatar_url'
+    );
+  elsif auth_provider = 'github' then
+    insert into public.profiles (id, email, username, name, avatar_url)
+    values (
+      new.id,
+      new.email,
+      new.raw_user_meta_data->>'user_name',
+      new.raw_user_meta_data->>'name',
+      new.raw_user_meta_data->>'avatar_url'
+    );
+end if;
+
+return new;
 end;
 $$ language plpgsql security definer;
